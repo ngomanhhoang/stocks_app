7568f590c15a81d619dc444fb5b9a04af5da4da7
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-11-19 21:05:08 +0100

    implement header

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Signalist",
-  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
+  description: "Track real-time stock prices, get personalized alerts and explore detailed company",
 };
 
 export default function RootLayout({
