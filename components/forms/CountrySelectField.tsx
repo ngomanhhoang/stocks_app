d24c05399f475ff974ae3409549d825f772ce7b5
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-11-22 21:10:09 +0100

    Implement components from shadcn

--- components/forms/CountrySelectField.tsx
+++ components/forms/CountrySelectField.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client";
 
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type CountrySelectProps = {
+  name: string;
+  label: string;
+  control: Control;
+  error?: FieldError;
+  required?: boolean;
+};
 const CountrySelectField = () => {
   return (
-    <div>CountrySelectField</div>
-  )
-}
+    <>
+      <Popover>
+        <PopoverTrigger>Open</PopoverTrigger>
+        <PopoverContent>Place content for the popover here.</PopoverContent>
+      </Popover>
 
-export default CountrySelectField
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </>
+  );
+};
+
+export default CountrySelectField;
